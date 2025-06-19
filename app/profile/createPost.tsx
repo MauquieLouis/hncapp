39cf0484778a97c2125c8b6a681fcfbf384f0974
MauquieLouis <louis.mauquie@gmail.com>
2025-06-19 23:18:38 +0200

    HNC-040 | allow open camera when creating post, and picture preview

--- app/profile/createPost.tsx
+++ app/profile/createPost.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import { Box } from "@/components/ui/box";
 import { Text } from "@/components/ui/text";
@@ -6,8 +6,14 @@ import { useLocalSearchParams } from 'expo-router';
 import { Ionicons } from "@expo/vector-icons";
 import { Input, InputField } from "@/components/ui/input";
 import ImagePostSelector from "@/components/profile/imagePostSelector";
+import { Textarea, TextareaInput } from "@/components/ui/textarea";
+import { HStack } from "@/components/ui/hstack";
 
 const CreatePost = () => {
+
+    const [ text, setText ] = useState("");
+    const [ vocal, setVocal ] = useState(null);
+    const [ legend, setLegend ] = useState(null);
 
     const { poster_id, user_id } = useLocalSearchParams();
 
@@ -18,35 +24,80 @@ const CreatePost = () => {
         // For example, you might want to fetch user data or initialize a form
     }, []);
 
+    useEffect(() => {
+        console.log("Legend changed: ", legend);
+    },[legend]);
+
     
 
 
     return (
         <Box style={{flex:1}}>
-            <Box style={{ flex:1, borderColor:"green", borderWidth:1, height:"100%"}}>
-                <Text>CREATE POST</Text>
+            <Box style={{ flex:1, height:"100%", justifyContent:"center", alignItems:"center", borderBottomWidth:1, borderColor:"grey", boxShadow:"0px 6px 12px rgba(0,0,0,0.1)" }}>
+                <Text
+                    size="3xl"
+                >
+                    Create a Post
+                </Text>
 
             </Box>
             <ImagePostSelector />
-            <Box style={{flex:4, borderColor:"pink", borderWidth:1, height:"100%"}}> 
+            <Box style={{flex:4, height:"100%", padding:10, borderTopWidth:1, borderColor:"grey", boxShadow:"0px 1px 8px rgba(0,0,0,0.7)"}}> 
                 {/* TEXT INPUT ZONE */}
-                <Input variant="outline" size="md" style={styles.writingInput}>
-                    <InputField 
-                        // ref={inputRef}
-                        // onFocus={() => setIsTextFocused(true)}
-                        // onBlur={() => setIsTextFocused(false)}
-                        // scrollEnabled={inputHeight >= maxHeight}
-                        placeholder="Write message here..." 
-                        // onChangeText={(text) => {setText(text); sendTypingEvent()}} 
-                        // value={text}
-                        multiline={true}
-                        // onContentSizeChange={handleContentSizeChange}
-                        style={{color:"black"}}
-                        />
-                </Input>
-                <Box>
-                    <Ionicons name="mic-circle-outline" size={50} color="black" />
-                </Box>
+                {legend === "text" ? 
+                <>
+                    <Textarea
+                    size="md"
+                    borderWidth={1}
+                    borderColor="$borderLight"
+                    borderRadius="$lg"
+                    height="$20" // 👈 Ensures 5 lines are visible
+                    p="$3"
+                    >
+                        <TextareaInput
+                            placeholder="Type your message here..."
+                            multiline
+                            textAlignVertical="top" // 👈 Ensures text starts at top
+                            />
+                    </Textarea>
+                    <Box style={{justifyContent: 'center', alignItems: 'center'}}>
+                        <TouchableOpacity onPress={() => { setLegend(null) }}>
+                            <Ionicons name="close-circle" size={46} color={"black"}/>
+                        </TouchableOpacity>
+                    </Box>
+                </>
+                :
+                <>
+                    {legend === "vocal" ? 
+                    <Box style={{justifyContent: 'center', alignItems: 'center'}}>
+                        <TouchableOpacity style={{ borderColor:'rgba(127,127,127,0.6)', borderWidth:3, padding:22, borderRadius:70 }}
+                                    onPress={() => setLegend("vocal")}>
+                                        <Ionicons name="mic-outline" size={70} color="rgba(127,127,127,0.8)" />
+                                    </TouchableOpacity>
+                        <TouchableOpacity onPress={() => { setLegend(null) }}>
+                            <Ionicons name="close-circle" size={46} color={"black"}/>
+                        </TouchableOpacity>
+                    </Box>
+                    :
+                    <Box style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                        <HStack space={"xl"}>
+                            <Box>
+                                <TouchableOpacity style={{ borderColor:'rgba(127,127,127,0.6)', borderWidth:3, padding:22, borderRadius:10 }} 
+                                onPress={() => setLegend("text")}>
+                                    <Ionicons name="text-outline" size={70} color="rgba(127,127,127,0.8)" />
+                                </TouchableOpacity>
+                            </Box>
+                            <Box>
+                                <TouchableOpacity style={{ borderColor:'rgba(127,127,127,0.6)', borderWidth:3, padding:22, borderRadius:10 }}
+                                onPress={() => setLegend("vocal")}>
+                                    <Ionicons name="mic-outline" size={70} color="rgba(127,127,127,0.8)" />
+                                </TouchableOpacity>
+                            </Box>
+                        </HStack>
+                    </Box>
+                    }
+                </>
+                }
             </Box>
         </Box>
     )
--- components/profile/imagePostSelector.tsx
+++ components/profile/imagePostSelector.tsx
@@ -1,113 +1,241 @@
 import { Ionicons } from "@expo/vector-icons";
-import React from "react";
-import { TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { TouchableOpacity, View, Image, Text } from "react-native";
 import { Box } from "@/components/ui/box";
 
 
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
-import AudioRecorder from './audioRecorder';
+import { decode } from 'base64-arraybuffer';
+import AudioRecorder from '@/components/conversations/audioRecorder';
 import { ImageManipulator, useImageManipulator } from 'expo-image-manipulator';
 import { Textarea, TextareaInput } from '../ui/textarea';
 import { TextInput } from 'react-native';
 import { v6 as uuidv6 } from 'uuid';
 import 'react-native-get-random-values';
+import { supabase } from "@/libs/initSupabase";
+import { ScrollView } from "react-native-gesture-handler";
+import { Video } from "expo-av";
+import * as DocumentPicker from 'expo-document-picker';
 
-const ImagePostSelector = () => {
+const ImagePostSelector = (user_id) => {
+
+    const [ media, setMedia ] = useState<ImagePicker.ImagePickerAsset[]>([]);
+     
 
     const pickImage = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ['images', 'videos'],
-            // allowsEditing: true,
-            aspect: [4,3],
-            quality:1,
-            base64: true,
-            allowsMultipleSelection: true,
-            selectionLimit: 10,
+        // let result = await ImagePicker.launchImageLibraryAsync({
+        //     mediaTypes: ['images', 'videos'],
+        //     // allowsEditing: true,
+        //     // aspect: [4,3],
+        //     quality:1,
+        //     base64: true,
+        //     allowsMultipleSelection: true,
+        //     selectionLimit: 9,
+        // });
+        // if(!result.canceled){
+        //     setMedia(result.assets);
+        //     // setImages(result.assets[0].uri);
+        //     // compressImage(result.assets[0].uri);
+        //     // uploadImage(result.assets);
+        // }
+        console.log("~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-");
+        console.log(" ~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-");
+        console.log("~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-");
+        console.log("\t")
+        console.log("\t Don't forget to change the image picker in this pickImage fct components/profile/imagePostSelector.tsx");
+        console.log("\t")
+        console.log("~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-");
+        console.log(" ~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-");
+        console.log("~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-~=*°*=~-");
+        const result = await DocumentPicker.getDocumentAsync({
+            type: ['image/*', 'video/*'],
+            multiple: true,
         });
+
+        console.log("RESULT :", result);
         if(!result.canceled){
+            setMedia(result.assets);
             // setImages(result.assets[0].uri);
             // compressImage(result.assets[0].uri);
-            uploadImage(result.assets);
+            // uploadImage(result.assets);
         }
     }
 
-    const uploadImage = async (files: ImagePicker.ImagePickerAsset[]) => {
-            try{
-                //Create the attachement and send Message
-                // for(let file in files as ImagePicker.ImagePickerAsset[]){
-                //     console.log("FILE :", files[file].fileName);
-                // }
-                // return;
-                setLoadingSend(true);
-                const fileNames = [];
-                for(let file of files as ImagePicker.ImagePickerAsset[]){
-                    // const filename = uuidv6();
-                    const ext = (file.fileName ?? '').split('.').pop();
-                    const fileName = uuidv6()+"."+ext;
-                    console.log("NEW FILENAME : ", fileName);
-                    fileNames.push(fileName);
-                    // console.log("FILE simple :", file);
-                    if(file.type == 'video'){
-                        const fileContent = await FileSystem.readAsStringAsync(file.uri, {encoding: FileSystem.EncodingType.Base64});
-                        const {data, error} = await supabase.storage.from('Conversations')
-                        .upload(convId+'/'+fileName, decode(fileContent),
-                        {cacheControl: '3600', upsert:false, contentType:file.mimeType});
-                        if(error){
-                            console.log("Error in uploadImage function when uploading new image in [...convId].tsx :", error);
-                        }
-                        console.log("DATA UPLOAD:", data);
-                    }else{
-                        console.log("NOT A VIDEO :");
-                        const fileResized = await compressImage(file.uri);
-                        console.log("FILE RESIZED :", fileResized?.uri);
-                        const fileContent = await FileSystem.readAsStringAsync(fileResized?.uri, {encoding: FileSystem.EncodingType.Base64});
-                        const {data, error} = await supabase.storage.from('Conversations')
-                        .upload(convId+'/'+fileName, decode(fileContent as string),
-                        {cacheControl: '3600', upsert:false, contentType:file.mimeType});
-                        if(error){ 
-                            console.log("Error in uploadImage function when uploading new image in [...convId].tsx :", error);
-                        }
-                        console.log("DATA UPLOAD:", data);
-                    }
-                    //Upload the file on supabase
-                }
-                const message_id = await sendTextMessage(true, 'attachment');
-                console.log("message iD :", message_id);
-                let index = 0;
-                for(let file of files as ImagePicker.ImagePickerAsset[]){
-                    const fileName = fileNames[index];
-                    index++;
-                    console.log("FILE :", file.fileName);
-                    const { data: attach_data, error: attach_error } = await supabase.from('attachments').insert({
-                        message_id: message_id,
-                        url: convId+'/'+fileName,
-                        type: file.mimeType,
-                        size: file.fileSize
-                    });
-                    if(attach_error){
-                        console.log("Error in uploadImage function when inserting new attachement in [...convId].tsx :", attach_error);
-    
-                    }
-                }
-                
-            }catch(error:unknown){
-                console.log("Error in uploadImage function [...convId].tsx :", error);
-            }finally{
-                setLoadingSend(false);
-            }
+    const openCamera = async () => {
+        const permissions = await ImagePicker.requestCameraPermissionsAsync();
+        if(!permissions.granted) {
+            console.log("Camera permissions not granted");
+            return;
         }
 
+        const result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ['videos', 'images'],
+            quality: 1,
+        });
+        console.log("RESULT :", result);
+        if(!result.canceled){
+            setMedia((prev) => [...prev, ...result.assets]);
+            // setImages(result.assets[0].uri);
+            // compressImage(result.assets[0].uri);
+            // uploadImage(result.assets);
+        }
+    }
+
+    const openVideo = async () => {
+        const permissions = await ImagePicker.requestCameraPermissionsAsync();
+        if(!permissions.granted) {
+            console.log("Camera permissions not granted");
+            return;
+        }
+
+        const result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ['videos'],
+            quality: 1,
+            videoMaxDuration: 60,
+        });
+        console.log("RESULT :", result);
+        if(!result.canceled){
+            setMedia((prev) => [...prev, ...result.assets]);
+            // setImages(result.assets[0].uri);
+            // compressImage(result.assets[0].uri);
+            // uploadImage(result.assets);
+        }
+    }
+
+    // const uploadImage = async (files: ImagePicker.ImagePickerAsset[]) => {
+    //         try{
+    //             //Create the attachement and send Message
+    //             // for(let file in files as ImagePicker.ImagePickerAsset[]){
+    //             //     console.log("FILE :", files[file].fileName);
+    //             // }
+    //             // return;
+    //             // setLoadingSend(true);
+    //             const fileNames = [];
+    //             for(let file of files as ImagePicker.ImagePickerAsset[]){
+    //                 // const filename = uuidv6();
+    //                 const ext = (file.fileName ?? '').split('.').pop();
+    //                 const fileName = uuidv6()+"."+ext;
+    //                 console.log("NEW FILENAME : ", fileName);
+    //                 fileNames.push(fileName);
+    //                 // console.log("FILE simple :", file);
+    //                 if(file.type == 'video'){
+    //                     const fileContent = await FileSystem.readAsStringAsync(file.uri, {encoding: FileSystem.EncodingType.Base64});
+    //                     const {data, error} = await supabase.storage.from('Conversations')
+    //                     .upload(convId+'/'+fileName, decode(fileContent),
+    //                     {cacheControl: '3600', upsert:false, contentType:file.mimeType});
+    //                     if(error){
+    //                         console.log("Error in uploadImage function when uploading new image in [...convId].tsx :", error);
+    //                     }
+    //                     console.log("DATA UPLOAD:", data);
+    //                 }else{
+    //                     console.log("NOT A VIDEO :");
+    //                     const fileResized = await compressImage(file.uri);
+    //                     console.log("FILE RESIZED :", fileResized?.uri);
+    //                     const fileContent = await FileSystem.readAsStringAsync(fileResized?.uri, {encoding: FileSystem.EncodingType.Base64});
+    //                     const {data, error} = await supabase.storage.from('Conversations')
+    //                     .upload(convId+'/'+fileName, decode(fileContent as string),
+    //                     {cacheControl: '3600', upsert:false, contentType:file.mimeType});
+    //                     if(error){ 
+    //                         console.log("Error in uploadImage function when uploading new image in [...convId].tsx :", error);
+    //                     }
+    //                     console.log("DATA UPLOAD:", data);
+    //                 }
+    //                 //Upload the file on supabase
+    //             }
+    //             const message_id = await sendTextMessage(true, 'attachment');
+    //             console.log("message iD :", message_id);
+    //             let index = 0;
+    //             for(let file of files as ImagePicker.ImagePickerAsset[]){
+    //                 const fileName = fileNames[index];
+    //                 index++;
+    //                 console.log("FILE :", file.fileName);
+    //                 const { data: attach_data, error: attach_error } = await supabase.from('attachments').insert({
+    //                     message_id: message_id,
+    //                     url: convId+'/'+fileName,
+    //                     type: file.mimeType,
+    //                     size: file.fileSize
+    //                 });
+    //                 if(attach_error){
+    //                     console.log("Error in uploadImage function when inserting new attachement in [...convId].tsx :", attach_error);
+    
+    //                 }
+    //             }
+                
+    //         }catch(error:unknown){
+    //             console.log("Error in uploadImage function [...convId].tsx :", error);
+    //         }finally{
+    //             setLoadingSend(false);
+    //         }
+    //     }
+
     return(
-        <Box style={{ flex:6, borderColor:"blue", borderWidth:1, height:"100%", padding:"10%"}}>
+        <Box style={{ flex:6, height:"100%", padding:"10%"}}>
             {/* IMAGE SELECTION ZONE */}
-            <TouchableOpacity 
-                onPress={() => {
-                    console.log("Open image picker or camera here");
-                }}
-            style={{ width:"100%",  height:"100%", borderColor:"grey", borderWidth:5, borderRadius:16, justifyContent:"center", alignItems:"center"}}>
-                <Ionicons name="camera-outline" size={86} color="grey"/>
-            </TouchableOpacity>
+            <Box style={{ justifyContent: 'center', alignItems: 'center', height:"15%", padding:1, marginBottom:16, flexDirection:"row"}}>
+                <TouchableOpacity 
+                    onPress={() => {
+                        console.log("Open video camera here");
+                        openVideo();
+                    }}
+                    style={{ width:"15%",  height:"100%", borderColor:"grey", borderWidth:5, borderRadius:16, justifyContent:"center", alignItems:"center"}}>
+                    <Ionicons name="videocam-outline" size={24} color="grey"/>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    onPress={() => {
+                        console.log("Open photo camera here");
+                        openCamera();
+                    }}
+                    style={{ width:"15%",  height:"100%", borderColor:"grey", borderWidth:5, borderRadius:16, justifyContent:"center", alignItems:"center"}}>
+                    <Ionicons name="camera-outline" size={24} color="grey"/>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    onPress={() => {
+                        console.log("Open image picker or camera here");
+                        pickImage();
+                    }}
+                    style={{ width:"15%",  height:"100%", borderColor:"grey", borderWidth:5, borderRadius:16, justifyContent:"center", alignItems:"center"}}>
+                    <Ionicons name="image-outline" size={24} color="grey"/>
+                </TouchableOpacity>
+            </Box>
+            <Box>
+                <ScrollView horizontal={true} >
+                    {media.map((item, index) => (
+                        <View key={index} style={{ marginRight: 16 }}>
+                            {item.mimeType?.startsWith('image') ? (
+                            // {item.type === 'image' ? (
+                            <>
+                            <Image
+                                source={{ uri: item.uri }}
+                                style={{ width: 220, height: 220, borderRadius: 8 }}
+                                />
+                            <Box style={{ position: 'absolute', top: 0, right: 0, padding: 4, backgroundColor: 'rgba(0, 0, 0, 0.3)', borderRadius: 8 }}>
+                                <TouchableOpacity onPress={() => {
+                                    setMedia(media.filter((_, i) => i !== index));
+                                }}>
+                                    <Ionicons name="close-circle" size={24} color="white" />
+                                </TouchableOpacity>
+                            </Box>
+                            </>
+                            ) : (
+                                // <Text>VIDEO</Text>
+                            <Video
+                                source={{ uri: item.uri }}
+                                rate={1.0}
+                                volume={1.0}
+                                isMuted={true}
+                                // resizeMode="cover"
+                                shouldPlay={false}
+                                style={{ width: 100, height: 100, borderRadius: 8 }}
+                            />
+                            )}
+                            <Text style={{ fontSize: 12, textAlign: 'center', marginTop: 4 }}>
+                            {item.type}
+                            </Text>
+                        </View>
+                        ))}
+                </ScrollView>
+            </Box>
         </Box>
     )
 }
