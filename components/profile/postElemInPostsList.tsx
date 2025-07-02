7fa5d2628a5e845045fd7426e61a32ab55d3d9e1
MauquieLouis <louis.mauquie@gmail.com>
2025-07-02 22:08:22 +0200

    HNC-047 | add like or dislike functionnality on post

--- components/profile/postElemInPostsList.tsx
+++ components/profile/postElemInPostsList.tsx
@@ -9,16 +9,23 @@ import { renderItem } from './render-item';
 import { useSharedValue, interpolate, Extrapolation } from 'react-native-reanimated';
 import AudioPlayer from '../conversations/audioPlayer';
 import UniversarlAudioPlayer from '../files/universalAudioPlayer';
+import { Ionicons } from '@expo/vector-icons';
+import { HStack } from '../ui/hstack';
+import { TouchableOpacity } from 'react-native';
+import { useUserContext } from '@/contexts/userContext';
 
 const PostElemInPostsList = (props: any) => {
 
     const [ urls, setUrls ] = useState([]);
     const [ audioUrl, setAudioUrl ] = useState<string | null>(null);
+    const [ isLiked, setIsLiked ] = useState(null);
 
     const item = props.item;
     const folder_url = props.folder_url;
     const bucket = props.bucket;
     const progress = useSharedValue<number>(0);
+
+    const { profile } = useUserContext();
 
     useEffect(()=> {
         console.log("ITEM :",item);
@@ -80,6 +87,66 @@ const PostElemInPostsList = (props: any) => {
         animated: true,
         });
     };
+
+    const togglePostReaction = async (postId: any, userId: any, action: any /* true=like, false=dislike */) => {
+        // Step 1: Check existing reaction
+        const { data: existing, error: fetchError } = await supabase
+            .from('post_likes')
+            .select('id, like')
+            .eq('post_id', postId)
+            .eq('user_id', userId)
+            .single();
+
+        if (fetchError && fetchError.code !== 'PGRST116') {
+            console.error('Fetch error:', fetchError);
+            return { error: fetchError };
+        }
+
+        // Step 2: If same reaction exists → delete
+        if (existing && existing.like === action) {
+            const { error: deleteError } = await supabase
+            .from('post_likes')
+            .delete()
+            .eq('id', existing.id);
+
+            if (deleteError) {
+            console.error('Delete error:', deleteError);
+            return { error: deleteError };
+            }
+
+            return { data: null, state: null };
+        }
+        // Step 3: Else insert or update (toggle to other reaction or create new)
+        const { data, error: upsertError } = await supabase
+            .from('post_likes')
+            .upsert(
+                {
+                post_id: postId,
+                user_id: userId,
+                liked_at: new Date().toISOString(),
+                like: action,
+                },
+            {
+                onConflict: 'post_id,user_id',
+            }
+            );
+
+        if (upsertError) {
+            console.error('Upsert error:', upsertError);
+            return { error: upsertError };
+        }
+
+        return { data, state: action };
+    };
+
+    const handleReaction = async (action: any) => {
+        const { state, error } =  await togglePostReaction(item.id, profile?.user_id, action);
+        if (!error) {
+            setIsLiked(state);
+            //  false, or null
+        }
+    }
+
 
     const ref = React.useRef<ICarouselInstance>(null);
     return (
@@ -155,9 +222,36 @@ const PostElemInPostsList = (props: any) => {
                     }}
                 />
             </Box>
-            <Box style={{width:"80%", marginLeft:"10%", borderTopColor:"white", borderTopWidth:1, }}>
+            <Box style={{width:"80%", marginLeft:"10%", borderBottomColor:"rgba(127,127,127,0.8)", borderBottomWidth:1, marginBottom:25, paddingBottom:10 }}>
+                <Box>
+                    <HStack style={{paddingVertical:15}} space={"xl"}>
+                        <TouchableOpacity onPress={async()=> {
+                            await handleReaction(true);
+                        }}>
+                            {isLiked
+                             ?
+                            <Ionicons name="heart-outline" size={34} color="cyan" />
+                            :
+                            <Ionicons name="heart-outline" size={34} color="white" />
+                            }
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={async()=> {
+                            await handleReaction(false);
+                        }}>
+                            {isLiked == true || isLiked == null
+                             ?
+                             <Ionicons name="skull-outline" size={32} color="white" />
+                             :
+                             <Ionicons name="skull-outline" size={32} color="red" />
+                            }
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Ionicons name="chatbubble-outline" size={32} color="white" />
+                        </TouchableOpacity>
+                    </HStack>
+                </Box>
                 {item.caption ?
-                    <Text>
+                    <Text style={{color:"rgba(180,180,180,0.8)"}}>
                         {item.caption}
                     </Text>
                 : 
